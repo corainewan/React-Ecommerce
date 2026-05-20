737ae036f479d5d8391d68ab7a858453d9d04e10
coraine <coraine@gmail.com>
2026-05-20 18:18:13 +0800

    create multiple pages using react router

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,8 +1,14 @@
 import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
 import "./App.css";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>Checkout Page</div>} />
+    </Routes>
+  );
 }
 
 export default App;
--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+// 就是 React 的严格模式，帮你检查代码有没有问题，只在开发时有效
+// 告诉 React："从这个 div 开始画页面！"，跟你之前用的 ReactDOM.createRoot 一样，只是写法不同！
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
