3c46c75a471160767375dc0d650ad69bc65509a3
coraine <coraine@gmail.com>
2026-05-24 21:36:52 +0800

    get cart from backend and display quantity

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import "./HomePage.css";
 
 export function HomePage() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
@@ -12,9 +13,15 @@ export function HomePage() {
     });
   }, []);
 
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
       <title>ECommerce Project</title>
 
