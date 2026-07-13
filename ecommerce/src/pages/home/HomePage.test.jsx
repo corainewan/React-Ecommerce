2a2e85f96ad17e9a7f47a289bb8c88d073d058f5
coraine <coraine@gmail.com>
2026-07-13 09:07:07 +0800

    Create integration test for HomePage

---
+++ ecommerce/src/pages/home/HomePage.test.jsx
@@ -0,0 +1,65 @@
+import { it, expect, describe, beforeEach, vi } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+// within = 在某个元素的范围内查找子元素
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router"; // MemoryRouter 是一个"假路由器"，它不会真的去改变浏览器地址栏的 URL，而是"假装"有一个路由器在运行，让你可以在测试环境里渲染出 <Link>、<NavLink>、<Route> 这些组件。
+import axios from "axios";
+import { HomePage } from "./HomePage";
+
+vi.mock("axios");
+
+describe("HomePage component", () => {
+  let loadCartMock;
+  beforeEach(() => {
+    loadCartMock = vi.fn();
+    axios.get.mockImplementation((url) => {
+      if (url === "/api/products") {
+        return {
+          data: [
+            {
+              id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+              image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+              name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+              rating: {
+                stars: 4.5,
+                count: 87,
+              },
+              priceCents: 1090,
+              keywords: ["socks", "sports", "apparel"],
+            },
+            {
+              id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+              image: "images/products/intermediate-composite-basketball.jpg",
+              name: "Intermediate Size Basketball",
+              rating: {
+                stars: 4,
+                count: 127,
+              },
+              priceCents: 2095,
+              keywords: ["sports", "basketballs"],
+            },
+          ],
+        };
+      }
+    });
+  });
+  it("display products correctly", async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCartMock} />
+      </MemoryRouter>,
+    );
+    const productContainers = await screen.findAllByTestId("product-container");
+    expect(productContainers.length).toBe(2);
+
+    expect(
+      within(productContainers[0]).getByText(
+        "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      ),
+    ).toBeInTheDocument();
+
+    expect(
+      within(productContainers[1]).getByText("Intermediate Size Basketball"),
+    ).toBeInTheDocument();
+  });
+});
