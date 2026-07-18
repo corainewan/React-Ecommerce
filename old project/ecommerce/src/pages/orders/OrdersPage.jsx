34f9e4074f4ee89ad82f349e0363932cb127edef
coraine <coraine@gmail.com>
2026-07-18 10:27:04 +0800

    Convert ecommerce project to Type Script

---
+++ old project/ecommerce/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,150 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState, useEffect, Fragment } from "react";
+import { Header } from "../../components/Header";
+import { formatMoney } from "../../utils/money";
+import "./OrdersPage.css";
+
+/*"id": "27cba69d-4c3d-4098-b42d-ac7fa62b7664",
+    "orderTimeMs": 1723456800000,
+    "totalCostCents": 3506,
+    "products": [{
+        "productId": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        "quantity": 1,
+        "estimatedDeliveryTimeMs": 1723716000000,
+        "product": {
+            "keywords": ["socks", "sports", "apparel"],
+            "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+            "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            "rating": {
+                "stars": 4.5,
+                "count": 87
+            },
+            "priceCents": 1090,
+            "createdAt": "2026-05-24T13:04:11.713Z",
+            "updatedAt": "2026-05-24T13:04:11.713Z"
+        }
+    }*/
+
+export function OrdersPage({ cart }) {
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header cart={cart} />
+      <div className="header">
+        <div className="left-section">
+          <a href="/" className="header-link">
+            <img className="logo" src="images/logo-white.png" />
+            <img className="mobile-logo" src="images/mobile-logo-white.png" />
+          </a>
+        </div>
+
+        <div className="middle-section">
+          <input className="search-bar" type="text" placeholder="Search" />
+
+          <button className="search-button">
+            <img className="search-icon" src="images/icons/search-icon.png" />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <a className="orders-link header-link" href="/orders">
+            <span className="orders-text">Orders</span>
+          </a>
+
+          <a className="cart-link header-link" href="/checkout">
+            <img className="cart-icon" src="images/icons/cart-icon.png" />
+            <div className="cart-quantity">{totalQuantity}</div>
+            <div className="cart-text">Cart</div>
+          </a>
+        </div>
+      </div>
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+                <div className="order-header">
+                  <div className="order-header-left-section">
+                    <div className="order-date">
+                      <div className="order-header-label">Order Placed:</div>
+                      <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
+                    </div>
+                    <div className="order-total">
+                      <div className="order-header-label">Total:</div>
+                      <div>{formatMoney(order.totalCostCents)}</div>
+                    </div>
+                  </div>
+
+                  <div className="order-header-right-section">
+                    <div className="order-header-label">Order ID:</div>
+                    <div>{order.id}</div>
+                  </div>
+                </div>
+
+                <div className="order-details-grid">
+                  {order.products.map((orderProduct) => {
+                    return (
+                      <Fragment key={orderProduct.product.id}>
+                        <div className="product-image-container">
+                          <img src={orderProduct.product.image} />
+                        </div>
+
+                        <div className="product-details">
+                          <div className="product-name">
+                            {orderProduct.product.name}
+                          </div>
+                          <div className="product-delivery-date">
+                            Arriving on:{" "}
+                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
+                              "MMMM D",
+                            )}
+                          </div>
+                          <div className="product-quantity">
+                            Quantity: {orderProduct.quantity}
+                          </div>
+                          <button className="buy-again-button button-primary">
+                            <img
+                              className="buy-again-icon"
+                              src="images/icons/buy-again.png"
+                            />
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
+                          </button>
+                        </div>
+
+                        <div className="product-actions">
+                          <a href="/tracking">
+                            <button className="track-package-button button-secondary">
+                              Track package
+                            </button>
+                          </a>
+                        </div>
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
