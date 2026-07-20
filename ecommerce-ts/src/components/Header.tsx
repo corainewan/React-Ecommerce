314c71619573e07e8f72194b5583b2873db1f008
coraine <coraine@gmail.com>
2026-07-20 12:52:10 +0800

    Convert money utils and Header to TypeScript

--- ecommerce-ts/src/components/Header.tsx
+++ ecommerce-ts/src/components/Header.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router";
 import "./header.css";
 
-export function Header({ cart }) {
+// type 这个关键字，是在给一坨类型定义取一个名字，之后哪里需要用到这个类型，就直接写 HeaderProps 代替，不用每次都重新把整坨物件结构写一遍。
+type HeaderProps = {
+  cart: {
+    productId: string;
+    quantity: number;
+    deliveryOption: string;
+  }[]; //{...} 定义"物件长什么样"，{...}[] 就是"很多个长这样的物件，放在一个数组里"
+};
+
+export function Header({ cart }: HeaderProps) {
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
